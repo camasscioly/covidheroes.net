75f0a3fa6613f3f09ebde00e1525c4b3e468a6e7
abai <mail.contraband@gmail.com>
2020-03-20 12:56:16 -0700

    Fix illegal path referencing

--- middleware/renderFile.js
+++ middleware/renderFile.js
@@ -1,8 +1,9 @@
 const ejs = require('ejs');
+const { join } = require('path');
 const { minify } = require('html-minifier');
 
 module.exports = (req, res, file, options = {}) => {
-  return ejs.renderFile(path.join(__dirname, `./../views/${file}.ejs`), options, (err, content) => {
+  return ejs.renderFile(join(__dirname, `./../views/${file}.ejs`), options, (err, content) => {
     if (err) return res.status(500).send(err);
     res.setHeader('Content-Type', 'text/html');
     res.send(

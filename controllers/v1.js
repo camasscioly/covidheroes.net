4364d95d643fd92da35c773c908c1b3593f8c8c1
abai <mail.contraband@gmail.com>
2020-03-20 15:42:23 -0700

    Revert query to body

--- controllers/v1.js
+++ controllers/v1.js
@@ -10,9 +10,9 @@ const router = Router();
 
 router.post('/us', auth, async (req, res) => {
   delete req.query.token;
-  await keyv.set('us', req.query);
-  console.log(req.query);
-  res.json(req.query);
+  await keyv.set('us', req.body);
+  console.log(req.body);
+  res.json(req.body);
 });
 
 router.get('/us', async (req, res) => {

aad59eabec03eb8716118852822d0542a6e69344
abai <mail.contraband@gmail.com>
2020-03-22 16:00:20 -0700

    Fix queryselector

--- public/header.js
+++ public/header.js
@@ -1,6 +1,6 @@
 if (!localStorage.getItem('member')) {
-  document.querySelector('account').innerHTML = `<button class="btn" onclick="window.location='https://covid.skywarder.cf/signup'">Sign up</button>
+  document.querySelector('#account').innerHTML = `<button class="btn" onclick="window.location='https://covid.skywarder.cf/signup'">Sign up</button>
   <button class="btn" onclick="window.location='https://covid.skywarder.cf/login'">Log in</button> `
 } else {
-  document.querySelector('account').innerHTML = `<button class="btn" onclick="window.location='https://covid.skywarder.cf/me'">Account</button>`
+  document.querySelector('#account').innerHTML = `<button class="btn" onclick="window.location='https://covid.skywarder.cf/me'">Account</button>`
 }

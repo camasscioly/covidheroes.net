73d1e71e8a2903404df22759cd484bd192b4416f
abai <mail.contraband@gmail.com>
2020-03-22 12:29:28 -0700

    Finish frontend for user system

---
+++ public/login.js
@@ -0,0 +1,34 @@
+const base = `${window.location.origin}/v1/`;
+
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *client
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return await response.json(); // parses JSON response into native JavaScript objects
+}
+
+
+document.querySelector('#login').onsubmit = () => {
+  postData(`${base}login`, { name: document.querySelector('#name').value, password: document.querySelector('#password').value })
+    .then((data) => {
+      const { name, email, phone, location, password } = data;
+      localStorage.setItem('name', name);
+      localStorage.setItem('email', email);
+      localStorage.setItem('phone', phone);
+      localStorage.setItem('location', location);
+      localStorage.setItem('password', password);
+      window.location = window.location.origin;
+    });
+  return false;
+}

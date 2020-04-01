7ca8de31b2da3a381532c84ab8b00de5176b426d
abai <mail.contraband@gmail.com>
2020-04-01 00:49:27 -0700

    Minor responsive adjustments

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,5 @@
-afterglow.play('teaser');
+var childCount = document.getElementById('scale').childElementCount;
+for (i = 0; i < childCount; i++) {
+  document.getElementById('scale').children[i].className = '';
+  document.getElementById('scale').children[i].className = 'col-xs-' + 12 / childCount;
+}

197a8b73f102988af8b52685a3ce4d5571613e2f
abai <mail.contraband@gmail.com>
2020-04-02 00:38:58 -0700

    Remove deprecated index.js code

--- public/js/index.js
+++
@@ -1,5 +0,0 @@
-var childCount = document.getElementById('scale').childElementCount;
-for (i = 0; i < childCount; i++) {
-  document.getElementById('scale').children[i].className = '';
-  document.getElementById('scale').children[i].className = 'col-xs-' + 12 / childCount;
-}

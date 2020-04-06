113a4cd49e4bb5f5d3d8ba30490a1595d8e4dda2
abai <mail.contraband@gmail.com>
2020-04-06 16:30:23 -0700

    Add support for canary

--- middleware/appPage.js
+++ middleware/appPage.js
@@ -3,7 +3,9 @@ module.exports = (req, res, next) => {
   const sub = parts[0] || null;
   const domain = parts[1];
   const type = `.${parts[2]}` || '';
-  const fixed = `${req.protocol}://${`app.${req.get('host').replace('www.', '')}`}${req.originalUrl}`;
-  if (sub === 'app') next();
+  const fixed = `${req.protocol}://${`app.${req.get('host').replace('www.', '')}`}${
+    req.originalUrl
+  }`;
+  if (sub === 'app' || req.get('host').includes('canary')) next();
   else res.redirect(fixed);
 };
--- middleware/normalPage.js
+++ middleware/normalPage.js
@@ -3,10 +3,11 @@ module.exports = (req, res, next) => {
   const sub = parts[0] || null;
   const domain = parts[1];
   const type = `.${parts[2]}` || '';
-  const fixed = `${req.protocol}://${`${req.get('host').replace(/^[^.]+\./g, '')}`}${req.originalUrl}`;
-  if (sub !== 'app') {
+  const fixed = `${req.protocol}://${`${req.get('host').replace(/^[^.]+\./g, '')}`}${
+    req.originalUrl
+  }`;
+  if (sub === 'app' || req.get('host').includes('canary')) {
     res.cookie('member', '', { expires: new Date(0) });
     next();
-  }
-  else res.redirect(fixed);
+  } else res.redirect(fixed);
 };

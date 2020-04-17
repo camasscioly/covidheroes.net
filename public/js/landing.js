abfb337957ed94df41f8b7e02201cf0cfeae87e8
abai <mail.contraband@gmail.com>
2020-04-16 19:43:40 -0700

    Fix font

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,3 +1,22 @@
+setTimeout(() => {
+  console.log(
+    '%cHold up!',
+    'color: #6C63FF; font-size: 4.5em; font-weight: bolder; text-shadow: #000 1px 1px;'
+  );
+  console.log(
+    "%cIf someone told you to copy/paste something here you have an 11/10 chance you're being scammed.",
+    'font-size: 150%;'
+  );
+  console.log(
+    '%cPasting anything in here could give attackers access to your COVID Heroes account.',
+    'color: red; font-size: 150%; font-weight: bold'
+  );
+  console.log(
+    '%cUnless you understand exactly what you are doing, close this window and stay safe.',
+    'font-size: 150%'
+  );
+}, 0);
+
 !(function () {
   'use strict';
   const e = document.getElementsByClassName('th');

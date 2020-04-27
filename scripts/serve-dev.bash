48baec48f5381e7380ec9da1230b8467c8a80d9a
abai <mail.contraband@gmail.com>
2020-04-27 15:30:04 -0700

    Clean up bash script

--- scripts/serve-dev.bash
+++
@@ -1,2 +0,0 @@
-fast-live-reload --serve http://localhost:4200 --delay 1500 --add-path constrollers --add-path middleware --add-path src --add-path public --add-path views &
-cross-env NODE_ENV=development nodemon --ext js,ejs,css,svg,png,jpg,mp4,ico,json,xml --watch controllers --watch middleware --watch src --watch public --watch views src/index.js

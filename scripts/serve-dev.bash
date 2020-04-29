5d187c6f1ade5feb52cd2833339d4ac5aed46683
Matthew Lancellotti <mvlancellotti@gmail.com>
2020-04-28 20:22:05 -0400

    fix constrollers typo

--- scripts/serve-dev.bash
+++ scripts/serve-dev.bash
@@ -1,2 +1,2 @@
-npx fast-live-reload --serve http://localhost:4200 --delay 1500 --add-path constrollers --add-path middleware --add-path src --add-path public --add-path views &
+npx fast-live-reload --serve http://localhost:4200 --delay 1500 --add-path controllers --add-path middleware --add-path src --add-path public --add-path views &
 npx nodemon --ext js,ejs,css,svg,png,jpg,mp4,ico,json,xml --watch controllers --watch middleware --watch src --watch public --watch views src/index.js

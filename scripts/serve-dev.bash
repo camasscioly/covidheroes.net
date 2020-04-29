c98eded620b6cea75c8f9e5740bdc93f3fd66520
abai <mail.contraband@gmail.com>
2020-04-29 11:59:09 -0700

    Add notes to gitignore

--- scripts/serve-dev.bash
+++ scripts/serve-dev.bash
@@ -1,2 +1,2 @@
 fast-live-reload --serve http://localhost:4200 --delay 1700 --add-path controllers --add-path middleware --add-path src --add-path public --add-path views --add-path locales &
-nodemon --ext js,ejs,css,svg,png,jpg,mp4,ico,json,xml --watch controllers --watch middleware --watch src --watch public --watch views --watch locales src/index.js
+cross-env NODE_ENV=development nodemon --ext js,ejs,css,svg,png,jpg,mp4,ico,json,xml --watch controllers --watch middleware --watch src --watch public --watch views --watch locales src/index.js
--- scripts/serve-dev.sh
+++
@@ -1,4 +0,0 @@
-RELOAD_PORT=3000
-
-cross-env fast-live-reload --serve http://localhost:$RELOAD_PORT --delay 1500 --add-path controllers --add-path middleware --add-path src --add-path public --add-path views &
-cross-env NODE_ENV=development nodemon --ext js,ejs,css,svg,png,jpg,mp4,ico,json,xml --watch controllers --watch middleware --watch src --watch public --watch views src/index.js
